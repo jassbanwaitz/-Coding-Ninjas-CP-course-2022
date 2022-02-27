31c88fd632a0d1a6c4a0ac1c008c27454d7f337a
jassbanwaitz <96936767+jassbanwaitz@users.noreply.github.com>
2022-02-27 14:32:11 +0530

    Update Power.cpp

--- Basics Of Recursion/Power.cpp
+++ Basics Of Recursion/Power.cpp
@@ -1,5 +1,7 @@
-The Code is Contributed by Jakaran Singh
+
 /*
+
+
 Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
 Do this recursively.
 Input format :
